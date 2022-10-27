1e7bb328bcb321a993d9f86600c4f06d1bde03b4
disquila <104382100+disquila@users.noreply.github.com>
2022-10-27 21:15:11 +0300

    Добавил счетчик и сделал ничью

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 const elemModal = document.querySelector('.modal');
 const restart = document.querySelector('#exampleModalToggle2');
 const table = document.querySelector('table');
-const gameBtn = document.querySelector('.btn-success').addEventListener('click', begin);
+const gameBtn = document.querySelector('.game').addEventListener('click', begin);
 
 let size = 0,
     win = 0,
-    winX = 0,
-    winO = 0,
+    counter = 0,
     whoseTurn = 1,
     arr = [];
 
@@ -18,21 +17,22 @@ window.addEventListener('load', () => {
 function begin() {
     size = Number(document.querySelector('.size').value);
     win = Number(document.querySelector('.win-line').value);
-    fillTable(table, createArr());
+    createArr();
+    fillTable();
     clickListen();
 };
 
 function createArr() {
-    arr = new Array(size);
+    arr = [];
     for (let i = 0; i < size; i++) {
         arr[i] = new Array(size);
-    }
-    return arr;
-}
+    };
+};
 
-function fillTable(table, arr) {
+function fillTable() {
     table.innerHTML = '';
-    for (j = 0; j < arr.length; j++) {
+
+    for (let j = 0; j < arr.length; j++) {
         const tr = table.insertRow();
 
         for (let k = 0; k < arr[j].length; k++) {
@@ -48,8 +48,6 @@ function fillTable(table, arr) {
                     td.classList.add('large');
                     break;
             };
-            arr[j][k] = '';
-            td.innerHTML = arr[j][k];
         };
     };
 };
@@ -57,74 +55,73 @@ function fillTable(table, arr) {
 function clickListen() {
     const cells = document.querySelectorAll('td');
 
-    for (m = 0; m < cells.length; m++) {
+    for (let m = 0; m < cells.length; m++) {
         cells[m].addEventListener('click', handleClick);
     };
 };
 
-function winner() {
-    if (winX === win || winO === win) {
-        whoseTurn = 1;
-        const modalRst = new bootstrap.Modal(restart);
-        document.querySelector('.winner').innerHTML = '';
-        modalRst.show();
-        document.querySelector('.winner').insertAdjacentHTML('afterbegin', `<p class="text-center fs-3">Победил ${event.srcElement.innerText}</p>`);
-    } else { winX = 0, winO = 0 };
+function handleClick(event) {
+    event.target.classList.add('no-click');
+    if (whoseTurn === 1) {
+        turn('cross', 'X', -1);
+        isWin();
+    } else {
+        turn('zero', 'O', 1);
+        isWin();
+    };
+};
+
+function turn(style, letter, wt) {
+    event.target.innerHTML = `<p class="text-td ${style}">${letter}</p>`;
+    arr[event.target.parentNode.rowIndex][event.target.cellIndex] = letter;
+    whoseTurn = wt;
 };
 
 function isWin() {
     // по горизонтали
-    for (let n = -(win - 1); n < 5; n++) {
-        if (arr[event.target.parentNode.rowIndex][event.target.cellIndex + n] === 1) {
-            winX++
-        };
-        if (arr[event.target.parentNode.rowIndex][event.target.cellIndex + n] === -1) {
-            winO++
+    for (let n = -(win - 1); n < win; n++) {
+        if (arr[event.target.parentNode.rowIndex][event.target.cellIndex + n] === event.target.innerText) {
+            counter++
         };
     };
-    winner()
+    endGame(`<p class="text-center fs-3">Победил ${event.target.innerText}</p>`);
 
     // по вертикали
-    for (let o = -(win - 1); o < 5; o++) {
+    for (let o = -(win - 1); o < win; o++) {
         if (arr[event.target.parentNode.rowIndex + o]) {
-            if (arr[event.target.parentNode.rowIndex + o][event.target.cellIndex] === 1) {
-                winX++
-            };
-            if (arr[event.target.parentNode.rowIndex + o][event.target.cellIndex] === -1) {
-                winO++
+            if (arr[event.target.parentNode.rowIndex + o][event.target.cellIndex] === event.target.innerText) {
+                counter++
             };
         };
     };
-    winner()
+    endGame(`<p class="text-center fs-3">Победил ${event.target.innerText}</p>`);
 
     // по диагонали
-    for (let p = -(win - 1); p < 5; p++) {
+    for (let p = -(win - 1); p < win; p++) {
         if (arr[event.target.parentNode.rowIndex + p]) {
-            if (arr[event.target.parentNode.rowIndex + p][event.target.cellIndex + p] === 1
-                || arr[event.target.parentNode.rowIndex + p][event.target.cellIndex - p] === 1) {
-                winX++
-            };
-            if (arr[event.target.parentNode.rowIndex + p][event.target.cellIndex + p] === -1
-                || arr[event.target.parentNode.rowIndex + p][event.target.cellIndex - p] === -1) {
-                winO++
+            if (arr[event.target.parentNode.rowIndex + p][event.target.cellIndex + p] === event.target.innerText
+                || arr[event.target.parentNode.rowIndex + p][event.target.cellIndex - p] === event.target.innerText) {
+                counter++
             };
         };
     };
-    winner()
-};
+    endGame(`<p class="text-center fs-3">Победил ${event.target.innerText}</p>`);
 
-function handleClick(event) {
-    if (whoseTurn === 1) {
-        turn('cross', 'X', 1, -1)
-        isWin();
-    } else {
-        turn('zero', 'O', -1, 1)
-        isWin();
+    // ничья
+    for (let q = 0; q < arr.length; q++) { 
+        if (arr[q].includes(undefined) === false) {
+            counter--
+        };
     };
+    if (counter === -size) {
+    endGame(`<p class="text-center fs-3">Ничья</p>`);}
 };
 
-function turn(style, letter, index, wt) {
-    event.target.innerHTML = `<p class="text-td ${style}">${letter}</p>`;
-    arr[event.target.parentNode.rowIndex][event.target.cellIndex] = index;
-    whoseTurn = wt;
-}
+function endGame(winner) {
+    if (counter === win || counter === -size) {
+        whoseTurn = 1;
+        const modalRst = new bootstrap.Modal(restart);
+        modalRst.show();
+        document.querySelector('.end-game').innerHTML = winner;
+    } else { counter = 0 };
+};
